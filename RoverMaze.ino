bd59cd59fb2e2bdd425a1eab024a8b6655978932
J. Davis <jwd3ca@gmail.com>
2025-03-12 12:32:04 -0700

    using NewPing library, no TOF

--- RoverMaze.ino
+++ RoverMaze.ino
@@ -5,32 +5,24 @@
 
 /* pseudo code:
       while(1)
-        find furthest wall
-        rotate TOF towards wall
-        calc stopDist
-        move towards wall until stopDist
-
-  crab_right();
-  forward();
-  crab_left();
-  do180();
-  // backward();
-  stop();
-
+        move forward until wall
+        look left, look right, choose direction to go
+        repeat
+        
   */
 
-
 #include <M5StickCPlus.h>  // w/o the 'Plus', Lcd stuff is broken
+#include <NewPing.h>
 #include <M5_RoverC.h>
 #include <Wire.h>
-#include "DFRobot_VL53L0X.h"
-// Create an instance of the VL53L0X sensor
-DFRobot_VL53L0X TOF;
 
-#define TRIG_PIN 33  // Trig pin connected to GPIO 33 (Grove port)
-#define ECHO_PIN 32  // Echo pin connected to GPIO 32 (Grove port)
+#define TRIG_PIN 33       // Trig pin connected to GPIO 33 (Grove port)
+#define ECHO_PIN 32       // Echo pin connected to GPIO 32 (Grove port)
+#define MAX_DISTANCE 200  // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
-// #define THREESTEP
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
+
+#define THREESTEP
 
 int topspeed = 50;
 int ms = 1000;
@@ -50,6 +42,7 @@ int textSize = 3;
 const int minAngle = 15;
 const int maxAngle = 160;
 int servoAngle = minAngle;  // initialize this to minAngle!
+int distance = 0;
 
 //==================================================================
 void setup() {
@@ -69,8 +62,6 @@ void setup() {
 
   Serial.println("Initializing devices...");
 
-  init_TOF();  // for looking aft Calling this breaks the servo at either 0 or 1!
-
   // Initialize RoverC
   roverc.begin(&Wire);  // Pass the Wire instance
 }
@@ -89,70 +80,22 @@ void update_sprite(int value, int x, int y) {
 void loop() {
 
   sprite.fillSprite(TFT_BLACK);  // Clear once per frame
+  show_volts();
 
 #ifdef THREESTEP
   three_step(17, 105, 165);
-
 #else
-  int fd = get_forwardDist();
-  update_sprite(fd, 20, 20);
-
-  int ad = get_aftDist();
-  update_sprite(ad, 20, 60);
-
-  show_volts();
+  distance = sonar.ping_cm();
+  update_sprite(distance, 20, 20);
 
 #endif
-
 
   sprite.pushSprite(0, 0);  // Once per frame only, raw the sprite covering the whole screen
 }
 
-
-//==================================================================
-int get_forwardDist() {
-  int forwardDist = 0;
-  long duration;
-  float distance;
-
-  // Send a 10µs pulse to trigger measurement
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
-
-  // Read the echo pulse duration
-  duration = pulseIn(ECHO_PIN, HIGH, 30000);  // Timeout 30ms
-
-  // Convert to distance (cm)
-  if (duration > 0) {
-    distance = duration * 0.0343 / 2;
-    // forwardDist = int(distance +0.5f);
-    forwardDist = int(distance);
-    Serial.printf("forwardDist: %d\n", forwardDist);
-  } else {
-    Serial.println("forwardDist out of range!");
-  }
-
-  delay(300);
-
-  fill_color = YELLOW;
-  return (forwardDist);
-}
-
-//-----------------------------------------------------------------------------------
-int get_aftDist() {
-  int aftDist = TOF.getDistance() / 10;
-  Serial.printf("aftDist: %d cm\n", aftDist);
-  delay(300);
-
-  fill_color = GREEN;
-  return (aftDist);
-}
-
 //-----------------------------------------------------------------------------------
 void show_volts() {
+  // loads the sprite but doesn't display it
   char String_buffer[128];
   float volts = M5.Axp.GetBatVoltage();
 
@@ -170,20 +113,6 @@ void show_volts() {
   sprite.print(String_buffer);
 }
 
-//==================================================================
-void init_TOF() {  // aft sensor, edited to switch to DF_Robot_VL53L0X.h
-  // this breaks the servo
-  Serial.println("Initializing TOF VL53L0X using chassis connector...");
-  Wire.begin(26, 0);  // SDA = 26,  SCL = 0 for the Grove connectors on RoverC chassis. Tested working if alone
-  TOF.begin(0x50);    // Use 0x50 for aft sensor via chassis port. Works
-  //Set to Back-to-back mode and high precision mode
-  TOF.setMode(TOF.eContinuous, TOF.eHigh);
-  //Laser rangefinder begins to work
-  TOF.start();
-  Serial.println("TOF VL53L0X initialized successfully");
-}
-
-
 //-----------------------------------------------------------------------------------
 void display(int degrees) {
   // Clear the screen before displaying new number
@@ -198,7 +127,6 @@ void crab_right() {
   roverc.setSpeed(topspeed, 0, 0);
   delay(ms);
 }
-
 
 void crab_left() {
   roverc.setSpeed(-topspeed, 0, 0);
@@ -235,23 +163,34 @@ void three_step(int a, int b, int c) {
   Serial.printf("in three_step() with %d %d %d\n", a, b, c);
 
   roverc.setServoAngle(1, a);  // rotate the motor counter-clockwise
-  forwardDist = get_forwardDist();
+  forwardDist = sonar.ping_cm();
+  Serial.printf("forwardDist: %d\n", forwardDist);
   sprite.fillSprite(TFT_BLACK);  // Clear once per frame
   update_sprite(forwardDist, 20, 20);
+    show_volts();
+
   sprite.pushSprite(0, 0);  // Once per frame only, raw the sprite covering the whole screen
   delay(3000);              // keep rotating for 5 seconds (5000 milliseconds)
 
   roverc.setServoAngle(1, b);  // rotate the motor counter-clockwise
-  forwardDist = get_forwardDist();
+  forwardDist = sonar.ping_cm();
+  Serial.printf("forwardDist: %d\n", forwardDist);
+
   sprite.fillSprite(TFT_BLACK);  // Clear once per frame
   update_sprite(forwardDist, 20, 20);
+    show_volts();
+
   sprite.pushSprite(0, 0);  // Once per frame only, raw the sprite covering the whole screen
   delay(3000);              // keep rotating for 5 seconds (5000 milliseconds)
 
   roverc.setServoAngle(1, c);  // rotate the motor counter-clockwise
-  forwardDist = get_forwardDist();
+  forwardDist = sonar.ping_cm();
+  Serial.printf("forwardDist: %d\n", forwardDist);
+
   sprite.fillSprite(TFT_BLACK);  // Clear once per frame
   update_sprite(forwardDist, 20, 20);
+    show_volts();
+
   sprite.pushSprite(0, 0);  // Once per frame only, raw the sprite covering the whole screen
   delay(3000);              // keep rotating for 5 seconds (5000 milliseconds)
 }
